bbf787accc5dab3b354cd3668d9179ec1c538289
Rahul Kumar <rahulkhg980@gmail.com>
2024-03-08 07:33:28 +0530

    lastcomponent graph was not coming , so fixed it

--- src/components/Lastcomponent.jsx
+++ src/components/Lastcomponent.jsx
@@ -94,46 +94,46 @@ function LastComponent() {
 }
 
 function CryptoCard({ cryptoData }) {
-    console.log("Sparkline URL:", cryptoData.sparkline);
-  
-    return (
-      <div className="w-[300px] h-[200px] rounded-2xl p-5 border-2 m-2">
-        <div className="flex items-center space-x-2">
-          <img
-            src={cryptoData.large}
-            alt={cryptoData.name}
-            className="w-6 h-6 rounded-full"
-          />
-          <span className="text-[16px] font-normal">{cryptoData.name}</span>
-          <span
-            className={`text-${
-              cryptoData.data.price_change_percentage_24h.usd > 0
-                ? "green"
-                : "red"
-            }-500 bg-${
-              cryptoData.data.price_change_percentage_24h.usd > 0
-                ? "#0AB27D"
-                : "#FF0000"
-            }/10 text-xs font-normal`}
-          >
-            {cryptoData.data.price_change_percentage_24h.usd.toFixed(2)}%
-          </span>
-        </div>
-        <div className="text-xl text-[#171717] font-medium mt-2">
-          {cryptoData.data.price}
-        </div>
-  
+  console.log("Sparkline URL:", cryptoData.data.sparkline);
+
+  return (
+    <div className="w-[300px] h-[200px] rounded-2xl p-5 border-2 m-2">
+      <div className="flex items-center space-x-2">
         <img
-          src={
-            cryptoData && cryptoData.sparkline
-              ? cryptoData.sparkline
-              : "https://www.coingecko.com/coins/33566/sparkline.svg"
-          }
-          alt={cryptoData && cryptoData.name ? cryptoData.name : ""}
-          className="w-full "
+          src={cryptoData.large}
+          alt={cryptoData.name}
+          className="w-6 h-6 rounded-full"
         />
+        <span className="text-[16px] font-normal">{cryptoData.name}</span>
+        <span
+          className={`text-${
+            cryptoData.data.price_change_percentage_24h.usd > 0
+              ? "green"
+              : "red"
+          }-500 bg-${
+            cryptoData.data.price_change_percentage_24h.usd > 0
+              ? "#0AB27D"
+              : "#FF0000"
+          }/10 text-xs font-normal`}
+        >
+          {cryptoData.data.price_change_percentage_24h.usd.toFixed(2)}%
+        </span>
       </div>
-    );
-  }
+      <div className="text-xl text-[#171717] font-medium mt-2">
+        {cryptoData.data.price}
+      </div>
+
+      <img
+        src={
+          cryptoData && cryptoData.data.sparkline
+            ? cryptoData.data.sparkline
+            : "https://www.coingecko.com/coins/33566/sparkline.svg"
+        }
+        alt={cryptoData && cryptoData.name ? cryptoData.name : ""}
+        className="w-full "
+      />
+    </div>
+  );
+}
 
 export default LastComponent;
